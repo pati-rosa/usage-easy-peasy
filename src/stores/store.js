0dea42b29bff4fd44046fa90542a25b08af70404
patirosa95 <patirosa995@gmail.com>
2021-02-10 00:33:06 -0300

    learning how to use easy peasy

---
+++ src/stores/store.js
@@ -0,0 +1,9 @@
+import { createStore, action } from 'easy-peasy';
+// import axios from 'axios';
+
+const store = createStore({
+    name: 'Vladimir',
+    courses: 'learn easy peasy'
+});
+
+export default store;
